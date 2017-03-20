c1dcca4f549abb684b062eece95e3d75ce5add09
filinov <filinov.victor@yandex.ru>
2017-03-20 16:11:30 +0500

    Если сообщение не было прочитано получателем до  удалении его отправителем, то оно удаляется с двух сторон

--- package/system/controllers/neomessenger/actions/delete_message.php
+++ package/system/controllers/neomessenger/actions/delete_message.php
@@ -17,29 +17,40 @@ class actionNeomessengerDeleteMessage extends cmsAction {
 
         foreach ($_message_ids as $message_id) {
 
-            $messege = $this->model->getItemById('{users}_messages', $message_id);
+            $message = $this->model->getItemById('{users}_messages', $message_id);
 
-            if (!$messege) { continue; }
-
-            $field = false;
+            if (!$message) { continue; }
 
             // Если сообщение отправил я
-            if ($messege['from_id'] == $user->id) {
-                $field = 'is_deleted_from';
+            if ($message['from_id'] == $user->id) {
+
+                if ($message['is_new']) {
+
+                    $this->model->update('{users}_messages', $message_id, array(
+                        'is_deleted_from' => 1,
+                        'is_deleted_to'   => 1
+                    ));
+
+                } else {
+
+                    $this->model->update('{users}_messages', $message_id, array(
+                        'is_deleted_from' => 1
+                    ));
+
+                }
+
             }
 
             // Если сообщение отправили мне
-            if ($messege['to_id'] == $user->id) {
-                $field = 'is_deleted_to';
-            }
+            if ($message['to_id'] == $user->id) {
 
-            if ($field) {
-
-                $this->model->update('{users}_messages', $message_id, array($field => 1));
-
-                $message_ids[] = (int)$message_id;
+                $this->model->update('{users}_messages', $message_id, array(
+                    'is_deleted_to' => 1
+                ));
 
             }
+
+            $message_ids[] = (int)$message_id;
 
         }
 
